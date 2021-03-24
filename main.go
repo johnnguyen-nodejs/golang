2a7f86689ad07c65b5c62451ef00ee7e663447b0
johnnguyen-nodejs <mrtuan1802@gmail.com>
2021-03-24 12:26:15 +0700

    v1

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"net/http"
 	"log"
 	"io/ioutil"
+	"github.com/johnnguyen-nodejs/golang/handlers"
 )
 
 func main() {
-	hh := handlers.NewHello()
+	l := log.New(os.Stdout, "product-api")
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 	http.ListenAndServe(":9090", nil)
 }
